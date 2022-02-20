40faaba675005184591441b56352b736ef99a7ed
BasmaSaid <eng.bs85@gmail.com>
2022-02-20 21:23:25 +0400

    Initial Commit

---
+++ tabView/tabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabViewApp.swift
+//  tabView
+//
+//  Created by Basma Said on 20/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct tabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
